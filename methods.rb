47fb551c3965e023b190b126900a8e4da6d4192f
alvinkenyagah <alvin47k@gmail.com>
2023-05-19 20:15:56 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,55 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+
+end
+
+greet_programmer
+
+
+
+def greet(name)
+   puts ("Hello, " + name + "!")
+end
+
+greet("Naureen")
+
+
+def greet_with_default(name = "programmer")
+    puts ("Hello, " + name + "!")
+end
+
+greet_with_default()
+
+
+
+def add(num1,num2)
+    
+ num1 + num2
+    
+end
+
+puts add(2,5)
+
+
+def halve(num1)
+    if num1.class != 0.class
+        nil
+    else 
+        
+        num1/2
+    end  
+    
+
+
+end
+
+puts halve(2)
+
+
+ name1 = "Bob"           ## We can only access variables in methods as arguments
+
+ def alert name 
+     puts "my name is " + name  
+ end 
+
+ alert(name1)
